e54fa8955da035f1b775f6113954da9b63be15f5
Arthur Attwell <arthur@arthurattwell.com>
2018-04-23 10:40:58 +0200

    Refine menu and footer placement

--- js/sticky-footer.js
+++ js/sticky-footer.js
@@ -12,41 +12,12 @@ function stickyFooter() {
     var footerHeight = window.innerHeight - contentDepth;
 
     if (footerDepth < window.innerHeight) {
-        // footer.style.position = 'absolute';
-        // footer.style.bottom = '0';
         footer.style.minHeight = footerHeight + 'px';
     }
 
 }
 
-// Check if images have all loaded.
-// Once they have, make the footer sticky.
-// https://stackoverflow.com/a/11071687/1781075
-function checkImagesLoaded() {
-
-    'use strict';
-
-    if ( document.images.length == 0 ) {
-        console.log('No images, sticking footer to bottom.');
-        stickyFooter();
-    } else {
-        var imgs = document.images;
-        var len = imgs.length;
-        var counter = 0;
-
-        [].forEach.call( imgs, function( img ) {
-            img.addEventListener( 'load', incrementCounter, false );
-        } );
-
-        function incrementCounter() {
-            counter++;
-            if ( counter === len ) {
-                console.log('Images loaded, sticking footer to bottom.');
-                stickyFooter();
-            }
-        }
-    }
-
-}
-
-checkImagesLoaded();
+window.addEventListener('load', function() {
+  console.log('All assets are loaded, adjusting footer accordingly.')
+  stickyFooter();
+})
